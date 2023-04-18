ce0144ab1aaec696ed287d4bf9fb6d7e6ebc117f
Yoonjin <yoonjin@Yoonjinui-MacBookAir.local>
2023-04-19 01:17:58 +0900

    create : LogoutDialog

--- app/src/main/java/com/example/usdividend/screen/SettingScreen.kt
+++ app/src/main/java/com/example/usdividend/screen/SettingScreen.kt
@@ -2,20 +2,22 @@ package com.example.usdividend
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material3.CenterAlignedTopAppBar
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.usdividend.screen.DividendDialog
+import com.example.usdividend.screen.LogoutDialog
 import com.example.usdividend.ui.theme.Gray
 import com.example.usdividend.ui.theme.Main
 import com.example.usdividend.ui.theme.Red
@@ -88,7 +90,18 @@ fun SettingAccount() {
                 .height(1.dp)
                 .background(Gray)
         )
-        TextButton(onClick = { /*TODO*/ }) {
+
+        var isClicked by remember {
+            mutableStateOf(false)
+        }
+
+        if (isClicked) {
+            LogoutDialog(v = true)
+        }
+
+        TextButton(onClick = {
+            isClicked = true
+        }) {
             Text(
                 text = stringResource(id = R.string.logout),
                 style = TextStyle(
